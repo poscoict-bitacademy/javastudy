17b818a36bf0a93574b8edc63b8d6beffb56044e
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-01-25 09:53:00 +0900

    Exception #1

---
+++ chapter03/src/main/java/exception/MyClass.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class MyClass {
+	public void danger() {
+		
+	}
+}
---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class MyClassTest {
+
+	public static void main(String[] args) {
+		new MyClass().danger();
+	}
+
+}
